da176d902540960963c0af76d1704d5b9dc87c9d
Joshua Reyes <reyes.j15@gmail.com>
2022-10-21 13:10:12 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 import { Route, Routes, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Switch >
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/rooms" component={ Rooms}/>
-        <Route exact path="/rooms/:slug" component={SingleRoom}/>     
-        <Route path="*" component={ Error}/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route path="*" component={Error} />
       </Switch>
     </div>
   );
