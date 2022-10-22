11559b1470e18ed484d5ab348cbf388a62a646b2
Joshua Reyes <reyes.j15@gmail.com>
2022-10-22 02:27:23 -0400

    updated context and singleRoom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import logo from "../images/logo.svg";
-import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import { FaAlignRight } from "react-icons/fa";
+import logo from "../images/logo.svg";
 export default class Navbar extends Component {
   state = {
-    isOpen: false,
+    isOpen: false
   };
   handleToggle = () => {
     this.setState({ isOpen: !this.state.isOpen });
@@ -14,7 +13,7 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar">
         <div className="nav-center">
-          <div className="nav-center">
+          <div className="nav-header">
             <Link to="/">
               <img src={logo} alt="Beach Resort" />
             </Link>
